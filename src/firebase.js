18044ab08dd5ecfc0d432f306310337f47a7b377
Lario <osunkunleolanrewaju@gmail.com>
2021-01-16 18:58:44 +0100

    project setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDUwf5PofQ_zRBUnnAHXjOZOfx3dZCD2QI",
+    authDomain: "challenge-530c9.firebaseapp.com",
+    projectId: "challenge-530c9",
+    storageBucket: "challenge-530c9.appspot.com",
+    messagingSenderId: "248601926849",
+    appId: "1:248601926849:web:cbf00b4e4bca9020670840",
+    measurementId: "G-2MTTHPP2RP"
+  };
