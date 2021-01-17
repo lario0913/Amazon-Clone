aa135734080aa42d1e271b22376f369f4da24013
Lario <osunkunleolanrewaju@gmail.com>
2021-01-17 22:27:29 +0100

    started working on the home component

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image"
+                src="/amazon-bg1.jpg" alt="amazon background" />
+            </div>
+        </div>
+    )
+}
+
+export default Home
