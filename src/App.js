9fa1046e8b0b28cd46dffe10c247c7f9c8807991
Lario <osunkunleolanrewaju@gmail.com>
2021-01-17 21:22:13 +0100

    completed the header component and the styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h2>We do this</h2>       
+      <Header />       
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 
 function Header() {
     return (
         <div className='header'>
             <img 
-                src="/images.png"
+                src="/amazon1.png"
                 alt="amazon symbol"
                 className="header__logo"
             />
 
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
-                {/* Logo */}
+                <SearchIcon className="header__searchIcon"/>
             </div>
 
             <div className="header__nav" >
@@ -27,6 +29,10 @@ function Header() {
                 <div className="header__option">
                     <span className="header__optionLineOne">Your</span>
                     <span className="header__optionLineTwo">Prime</span>
+                </div>
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__OptionLineTwo header__BasketCount">0</span>
                 </div>
             </div>
         </div>
