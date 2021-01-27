fa8c6192d59d1bf51fb15583755dcea7f554f284
Lario <osunkunleolanrewaju@gmail.com>
2021-01-27 22:39:26 +0100

    started implementing stripe payment for the application

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Login from './Login';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
 import Payment from './Payment';
+import {loadStripe} from '@stripe/stripe-js'
+import {Elements} from "@stripe/react-stripe-js"
+
+const promise = loadStripe("pk_test_yddzv5xgxyiIqcxetkxtIJRo0016zZnjNa")
 
 function App() {
   const[{user}, dispatch] = useStateValue()
@@ -45,7 +49,9 @@ function App() {
 
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
 
           <Route path="/" >
--- src/Payment.js
+++ src/Payment.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Payment.css'
 import { useStateValue } from './StateProvider'
 import CheckoutProduct from './CheckoutProduct'
 import {Link} from 'react-router-dom'
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
+import CurrencyFormat from 'react-currency-format'
+import { getBasketTotal } from './reducer'
 
 function Payment() {
     const [{basket, user}] = useStateValue()
+    
+    const stripe = useStripe()
+    const elements = useElements()
+
+    const [error, setError] = useState(null)
+    const [disabled, setDisabled] = useState(true)
+    const [processing, setProcessing] = useState(false)
+    const [succeeded, setSucceeded] = useState(false)
+
+    const handleSubmit = e => {
+
+    }
+
+    const handleChange = e => {
+        setDisabled(e.empty)
+        setError(e.error? e.error.message : "")
+    }
 
     return (
         <div className="payment">
@@ -35,11 +55,36 @@ function Payment() {
                     </div>
                 </div>
                 <div className="payment__section">
-                    <div className="payment title">
+                    <div className="payment__title">
                         <h3>Payment Method</h3>
                     </div>
                     <div className="payment__details">
+                        <form onSubmit={handleSubmit}>
+                            <CardElement onChange={handleChange} />
 
+                            <div className="payment__priceContainer">
+                                <CurrencyFormat 
+                                    renderText ={value => (
+                                        <>
+                                            <p>
+                                                <strong>Order Total: {value}</strong>
+                                            </p>
+                                        </>
+                                    )}
+                                    decimal={2}
+                                    value={getBasketTotal(basket)}
+                                    thousandSeparator={true}
+                                    displayType={"text"}
+                                    prefix={"$"}
+                                />
+                                <button disabled={processing || disabled ||
+                                succeeded}>
+                                    <span>{processing ? <p>Processing</p> : "BUY NOW"}</span>
+                                </button>
+                            </div>
+
+                            {error && <div>{error}</div>}
+                        </form>
                     </div>
                 </div>
             </div>
