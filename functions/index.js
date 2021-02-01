ced76e0de3112ae85d3a08032ad0ff97bf7cb134
Lario <osunkunleolanrewaju@gmail.com>
2021-02-01 11:37:06 +0100

    backend of the application completed and deployed with firebase

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const { request } = require("express");
+// const {request} = require("express");
 const stripe = require("stripe")("sk_test_mBzNd2PYngAHGoVRZ9eQ02nU00vvXcn8Mu");
 
 
@@ -18,17 +18,17 @@ app.use(express.json());
 // API routes
 app.get("/", (req, res) => res.status(200).send("helloword")
 );
-app.post("/payments/create", async (req, res) => {
-    const total = req.query.total;
-    console.log("Payment request received: ", total);
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: total,
-        currency: "usd"
-    });
-    res.status(201).send({
-        clientSecret: paymentIntent.client_secret
-    });
-})
+app.post("/payments/create", async (req, res) =>{
+  const total = req.query.total;
+  console.log("Payment request received: ", total);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
+  });
+  res.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
 // Listen Command
 exports.api = functions.https.onRequest(app);
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/challenge-530c9/us-central1/api'
+    baseURL: 'https://us-central1-challenge-530c9.cloudfunctions.net/api'
+    // 'http://localhost:5001/challenge-530c9/us-central1/api'
 })
 
 export default instance
