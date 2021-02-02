735a2681a405355635adcf3bc738fa13427b755f
Lario <osunkunleolanrewaju@gmail.com>
2021-02-02 13:07:30 +0100

    new edition of the app is build and deployed

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    // baseURL: 'https://us-central1-challenge-530c9.cloudfunctions.net/api'
-    baseURL: 'http://localhost:5001/challenge-530c9/us-central1/api'
+    baseURL: 'https://us-central1-challenge-530c9.cloudfunctions.net/api'
+    // baseURL: 'http://localhost:5001/challenge-530c9/us-central1/api'
 })
 
 export default instance
